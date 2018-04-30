a2d3cb42e6ebf94314849681977f2fb1844e77c0
Drew.Jex <drew.jex@ITDEV-06.local>
2018-04-30 09:08:12 -0600

    optimized scrolling mobile

--- app/js/index.js
+++ app/js/index.js
@@ -1,11 +1,19 @@
 import "./imports";
 
-if (navigator.msMaxTouchPoints) {
-    document.body.classList.add("ms-touch");
+document.addEventListener('DOMContentLoaded', () => {
+  setTimeout(() => {
+    window.scrollTo(0, 0);
+    document.getElementById('slider').style.height = `${Array.from(document.querySelectorAll(".container"))[0].offsetHeight}px`;
+    document.getElementById('slider').style.overflow = 'hidden';
+  }, 500);
+}, false);
 
-    document.body.addEventListener("scroll", event => {
-        document.querySelector('.slide-image').style.transform = `translate3d(-${(100-$(this).scrollLeft()/6)}px,0,0)`;
-    });
+if (navigator.msMaxTouchPoints) {
+    // document.body.classList.add("ms-touch");
+
+    // document.body.addEventListener("scroll", event => {
+    //     document.querySelector('.slide-image').style.transform = `translate3d(-${(100-$(this).scrollLeft()/6)}px,0,0)`;
+    // });
 } else {
 
     let slider = {
@@ -24,8 +32,6 @@ if (navigator.msMaxTouchPoints) {
       index: 0,
       longTouch: undefined,
       lastTouch:0,
-      moveDown:true,
-
       heightValues: [],
       
       init: function() {
@@ -66,22 +72,6 @@ if (navigator.msMaxTouchPoints) {
     
         return yPosition;
     },
-
-    dw_getScrollOffsets: function() {
-        var doc = document, w = window;
-        var x, y, docEl;
-        
-        if ( typeof w.pageYOffset === 'number' ) {
-            x = w.pageXOffset;
-            y = w.pageYOffset;
-        } else {
-            docEl = (doc.compatMode && doc.compatMode === 'CSS1Compat')?
-                    doc.documentElement: doc.body;
-            x = docEl.scrollLeft;
-            y = docEl.scrollTop;
-        }
-        return {x:x, y:y};
-    },
   
       start: function(event) {
         // Test for flick.
@@ -92,97 +82,41 @@ if (navigator.msMaxTouchPoints) {
   
         // Get the original touch position.
         this.touchstartx = event.changedTouches[0].pageX;
-        this.touchstarty =  this.dw_getScrollOffsets().y; //event.changedTouches[0].pageY; //window.pageYOffset || document.documentElement.scrollTop;
-        //this.slideHeight = document.querySelector(".container").offsetHeight;
-
-       // console.log("START:"+this.touchstarty);
-
-        console.log("STARTX :"+this.touchstartx);
   
         // The movement gets all janky if there's a transition on the elements.
-        document.querySelector('.animate').classList.remove('animate');
+        if (document.querySelector('.animate') != null) {
+          document.querySelector('.animate').classList.remove('animate');
+        }
       },
 
       move: function(event) {
-        //console.log(this.dw_getScrollOffsets());
+        
       },
   
       end: function(event) {
-        // Calculate the distance swiped.
-        // var absMove = Math.abs(this.index*this.slideHeight - this.movey);
-        // // Calculate the index. All other calculations are based on the index.
-        // if (absMove > this.slideHeight/2 || this.longTouch === false) {
-        //   if (this.movey > this.index*this.slideHeight && this.index < 2) {
-        //     this.index++;
-        //   } else if (this.movey < this.index*this.slideHeight && this.index > 0) {
-        //     this.index--;
-        //   }
-        // }  
 
-        let touchEnd = this.dw_getScrollOffsets().y; //event.changedTouches[0].pageY; //window.pageYOffset || document.documentElement.scrollTop;
+        console.log("HIT");
+
         let touchEndx = event.changedTouches[0].pageX;
-
-        // if (touchEnd > this.touchstarty) {
-        //     //down
-        //     if (this.index < document.querySelectorAll(".container").length-1)
-        //         this.index++;
-        // } else {
-        //     //up
-        //     if (this.index > 0)
-        //         this.index--;
-        // } 
-
-        //console.log("END:"+touchEnd);
-
-        //console.log("ENDX "+touchEndx);
-
-        //console.log(this.index);
-        
-        //this.slideHeight = //this.heightValues[this.index+1]; //this.getPosition(document.querySelectorAll(".container")[this.index]); //document.querySelectorAll(".container")[this.index].offsetHeight;
-
         let swipeX = touchEndx - this.touchstartx;
 
         if (swipeX < -50) {
             //left down
-            if (this.index < document.querySelectorAll(".container").length-1)
-                this.index++;
+            if (this.index < document.querySelectorAll(".container").length-1) {
+              this.index++;
+              window.scrollTo(0, 0);
+            }
         } else if (swipeX > 50) {
-            if (this.index > 0)
-                 this.index--;
+            if (this.index > 0) {
+              this.index--;
+              window.scrollTo(0, 0);
+            }
         }
 
-        let diff = (window.pageYOffset || document.documentElement.scrollTop) - this.heightValues[this.index];
-
-        //here's the issue, then it should work.
-        console.log(this.heightValues[this.index]);
-
-        console.log((window.pageYOffset || document.documentElement.scrollTop));        
-        
-        console.log("DIFF: "+diff);
-
-        //remove ticket image on mobile
-        //diff = 0;
-
-        //console.log((window.pageYOffset || document.documentElement.scrollTop) - this.heightValues[this.index]);
-
-        this.slideHeight = (document.querySelectorAll(".container")[this.index].offsetHeight - diff)*this.index;
-
-        //console.log(this.slideHeight);
-        //console.log(this.heightValues[this.index]);
-
-        //this.index++; 
-        // Move and animate the elements.
-        //this.el.holder.addClass('animate').css('transform', 'translate3d(-' + this.index*this.slideHeight + 'px,0,0)');
         this.el.holder.classList.add('animate');
-        this.el.holder.style.transform = `translate3d(0,-${this.slideHeight}px,0)`;
-
-        //when animation ends, diff should be zero again..
-
-        //console.log(this.index*this.slideHeight);
-        //this.el.imgSlide.addClass('animate').css('transform', 'translate3d(-' + 100-this.index*50 + 'px,0,0)');
-        //this.el.imgSlide.classList.add('animate');
-        //this.el.imgSlide.style.transform = `translate3d(0,-${100-this.index*50}px,0)`;
-  
+        this.el.holder.style.transform = `translate3d(0,-${this.heightValues[this.index]}px,0)`;
+        document.getElementById('slider').style.height = `${Array.from(document.querySelectorAll(".container"))[this.index].offsetHeight}px`;
+        document.getElementById('slider').style.overflow = 'hidden';
       }
   
     };
